9c507503f45226486f5bb1d6b5554ae653f242b4
lvink <865810953@qq.com>
2016-07-29 17:54:49 +0800

    项目

--- weisheng_dati/clear.php
+++
@@ -1,7 +0,0 @@
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<?php
-	setcookie("phone", "", time()-3600,"/");
-	setcookie("username", "", time()-3600,"/");
-	setcookie("sc", "", time()-3600,"/");
-	setcookie("nphone", "", time()-3600,"/");
-	echo "清理完成";
--- weisheng_dati/getflashcode.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	$time = time();
-	$src = '/plus/leen/flashcode.php?t=' . $time;
-	echo '<img src="' . $src .'" style="width:80px;heigth:30px;"/>';
-
-?> 
